422218d684e946769c4a772b34b43c7cddde4210
Andres Vargas Lugo <andres.vargas@avawomen.com>
2018-12-08 16:18:48 +0100

    deployed contract in Rikeby

--- Project_5/Project 5 - Boilerplate with Zeppelin/smart_contracts/truffle.js
+++ Project_5/Project 5 - Boilerplate with Zeppelin/smart_contracts/truffle.js
@@ -1,10 +1,10 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
@@ -12,19 +12,23 @@
  *   },
  */
 
+
+
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var mnemonic = 'parent leopard equip settle gadget used acquire absorb reject unaware force annual'
+var url = 'https://rinkeby.infura.io/v3/df52c8543c084c78a9dd90cd46ca3752'
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "localhost",
+      host: '127.0.0.1',
       port: 8545,
-      network_id: "*" // Match any network id
-    }
-
-rinkeby: {
-      provider: function() { 
-        return new HDWalletProvider(mnemonic, 'PASTE URL HERE') 
+      network_id: "*"
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, url)
       },
       network_id: 4,
       gas: 4500000,
