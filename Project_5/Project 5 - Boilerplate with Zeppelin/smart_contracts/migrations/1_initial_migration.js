082a788593d18d0a6a1f8dc7353b910bf41ba852
Andres Vargas Lugo <andres.vargas@avawomen.com>
2018-12-09 15:39:07 +0100

    created file for interacting with smart contract

--- Project_5/Project 5 - Boilerplate with Zeppelin/smart_contracts/migrations/1_initial_migration.js
+++ Project_5/Project 5 - Boilerplate with Zeppelin/smart_contracts/migrations/1_initial_migration.js
@@ -3,3 +3,9 @@ var Migrations = artifacts.require("./Migrations.sol");
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
 };
+
+var StarNotary = artifacts.require("./StarNotary.sol");
+
+module.exports = function(deployer) {
+    deployer.deploy(StarNotary);
+};
